dc093bb20693b039d9d3b4ef5bba6992df1ff892
Unknown-0perator <ahmadrashidakhtar@gmail.com>
2022-05-21 20:28:55 -0700

    Pixel created

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 USERNAME = 'unknown0perator'
 TOKEN = 'SomethingRandom'
 pixela_endpoint = 'https://pixe.la/v1/users'
@@ -22,5 +23,14 @@ graph_config = {
     'type': 'int',
     'color': 'momiji',
     }
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{graph_endpoint}/{graph_config['id']}"
+today_date = datetime.now()
+pixel_data = {
+    'date': today_date.strftime('%Y%m%d'),
+    'quantity': '5',
+}
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
